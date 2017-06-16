417068ff865944712c98317e24d8dd32281ebba7
tinh Nguyen <tinhdev123@gmail.com>
2017-06-16 23:50:17 +0700

    comment update

--- application/controllers/Washington.php
+++ application/controllers/Washington.php
@@ -2,38 +2,35 @@
 
 class Washington extends CI_Controller {
 
-	public function __construct() {
+		public function __construct() {
+			parent::__construct();
+			$this->load->helper('url');
+			$this->load->library('session');
 
-		parent::__construct();
-		$this->load->helper('url');
-		$this->load->library('session');
-
-		// to protect the controller to be accessed only by registered users
-	    if(!$this->session->userdata('logged_in')){
-
-			redirect('login', 'refresh');
-
-		}
+			// to protect the controller to be accessed only by registered users
+		    if(!$this->session->userdata('logged_in')){
+					redirect('login', 'refresh');
+			}
 
 	}
 
 	public function index() {
-
-		$data['content'] = 'dashboard';
-		$this->load->view('base_view', $data);
+		//to display dashboard page as default.
+			$data['content'] = 'dashboard';
+			$this->load->view('base_view', $data);
 
 	}
 
 	public function dashboard() {
-
-		$data['content'] = 'dashboard';
-		$this->load->view('base_view', $data);
+		//to display when selected dashboard page.
+			$data['content'] = 'dashboard';
+			$this->load->view('base_view', $data);
 	}
 
 	public function newsfeed() {
-
-		$data['content'] = 'newsfeed';
-		$this->load->view('base_view', $data);
+		//to display when selected newsfeed page.
+			$data['content'] = 'newsfeed';
+			$this->load->view('base_view', $data);
 	}
 
 }
--- application/models/Login_auth_db.php
+++ application/models/Login_auth_db.php
@@ -1,51 +1,52 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-
+// Model for login auth
 Class Login_auth_db extends CI_Model {
-	
+
 	public function __construct() {
-		
+
 		parent::__construct();
 		$this->load->database();
-		
+
 	}
-	
+
+
+	//login process
 	function login($username, $password) {
-		
+
 		$this->db->select('*');
 		$this->db->from('users');
 		$this->db->where('username', $username);
 		$this->db->where('password', $password);
 		$query = $this->db->get()->row();
-		
+
 		if (!empty($query)) {
-			
+
 			return true;
-			
+
 		} else {
-			
+
 			return false;
-			
+
 		}
-		
+
 	}
-	
+
+	//getting user data
 	function get_user_data($username) {
-		
-		//$this->load->library('session');
 		$this->db->select('*');
 		$this->db->from('users');
-		$this->db->where('username', $username);	
+		$this->db->where('username', $username);
 		$query = $this->db->get();
-		
+
 		$user_data = array(
 			'username'	=> $query->row('username'),
 			'status'	=> $query->row('status'),
 			'name'	=> $query->row('name'),
 			'level'	=> $query->row('level')
 			);
-		
+
 		return $user_data;
-		
+
 	}
-	
+
 }
